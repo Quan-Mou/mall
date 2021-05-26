7922e749cddb3d18c4e5ed247c7450bd7aa76a4b
Root_QuanMou <2312038309@qq.com>
2021-05-26 13:52:20 +0800

    购物车

--- src/views/cart/childcomps/CartBottom.vue
+++ src/views/cart/childcomps/CartBottom.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="bottom">
-    <check-button></check-button>
+    <div class="cart-item-bottom" @click="CheckBottom">
+      <check-button></check-button>
+      <span>全选</span>
+    </div>
+    <span class="total-price">合计: {{}}</span>
     <span class="settlement right">
       结算({{ this.$store.state.CartList.length }})
     </span>
-    <span class="total-price right">合计: {{}}</span>
   </div>
 </template>
 
@@ -13,14 +16,32 @@ import CheckButton from "./CheckButton.vue";
 export default {
   name: "CartBottom",
   components: { CheckButton },
+  methods: {
+    CheckBottom() {
+      console.log("点击了");
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.cart-item-bottom {
+  display: flex;
+  width: 70px;
+  height: 30px;
+  align-content: center;
+  line-height: 18px;
+  justify-content: center;
+  margin: 12px 0;
+}
+.cart-item-bottom span {
+  margin-left: 4px;
+}
 .bottom {
   height: 44px;
-  background-color: black;
+  background-color: #eeeeee;
   position: absolute;
+  display: flex;
   left: 0;
   bottom: 49px;
   width: 100%;
@@ -29,6 +50,8 @@ export default {
 .total-price {
   margin-right: 10px;
   font-size: 14px;
+  flex: 1;
+  margin-left: 20px;
 }
 .settlement {
   width: 85px;
@@ -40,5 +63,6 @@ export default {
   border-radius: 15px;
   margin: 7px 10px 0 0;
   font-size: 14px;
+  flex: 1;
 }
 </style>
